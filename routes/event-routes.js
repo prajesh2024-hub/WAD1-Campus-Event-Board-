c4099f8ad0f73477d1d705a09925214679218926
Matthew Tjandera <matthewtjandera@gmail.com>
2026-03-25 15:00:48 +0800

    clean up

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -4,12 +4,18 @@ const eventController = require('../controllers/event-Controller');
 
 const router = express.Router(); // sub application
 
+//root home page
 router.get("/", eventController.getHome);
 
+
+//create events and showing all events created
 router.get("/create-event", eventController.getCreateEvent);
 router.post("/create-event", eventController.postCreateEvent);
+
+//shows all events
 router.get("/all-events", eventController.allEvents);
 
+//show all events created
 router.get("/my-events", eventController.eventList);
 
 //edit events
@@ -20,6 +26,7 @@ router.post("/events/:id/edit", eventController.postEditEvent);
 router.get("/events/:id/delete", eventController.getDeleteEvent);
 router.post("/events/:id/delete", eventController.deleteEvent);
 
+//getting event details
 router.get("/events/:id", eventController.getEventDetails);
 // EXPORT
 module.exports = router;
